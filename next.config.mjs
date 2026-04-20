6686d6c28ccb9cdede17740786819d6f6f11aab9
Trésork-007 <tresoressene@gmail.com>
2026-04-20 23:18:49 +0100

    Configuration pour GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/portefolio-franky' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portefolio-franky' : '',
 }
 
 export default nextConfig
